0f803feaa517a2c108b82557e55a75b900507490
yuiseki <yuiseki@gmail.com>
2021-06-13 20:33:41 +0900

    ナウキャストを追加する

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -106,6 +106,9 @@ const Map = () => {
       shadowUrl: 'images/marker-shadow.png',
     });
   }, []);
+  const time = '20210613112000';
+  const baseTime = time;
+  const validTime = time;
   return (
     <MapContainer
       scrollWheelZoom={true}
@@ -123,12 +126,24 @@ const Map = () => {
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           />
         </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name='国土地理院色別標高図'>
+        <LayersControl.Overlay name='ナウキャスト'>
+          <TileLayer
+            attribution='<a href="https://maps.gsi.go.jp/development/ichiran.html#relief">国土地理院色別標高図</a>'
+            url={
+              'https://www.jma.go.jp/bosai/jmatile/data/nowc/' +
+              baseTime +
+              '/none/' +
+              validTime +
+              '/surf/hrpns/{z}/{x}/{y}.png'
+            }
+          />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name='国土地理院色別標高図'>
           <TileLayer
             attribution='<a href="https://maps.gsi.go.jp/development/ichiran.html#relief">国土地理院色別標高図</a>'
             url='https://cyberjapandata.gsi.go.jp/xyz/relief/{z}/{x}/{y}.png'
           />
-        </LayersControl.BaseLayer>
+        </LayersControl.Overlay>
         <FireDept />
       </LayersControl>
     </MapContainer>
