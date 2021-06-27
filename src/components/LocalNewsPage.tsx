557f1ac10e7d4c2483944c9ca9bc3e1ae4052dbb
yuiseki <yuiseki@gmail.com>
2021-06-27 15:18:11 +0900

    add select category capability

--- src/components/LocalNewsPage.tsx
+++ src/components/LocalNewsPage.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 import tw from 'twin.macro';
 import { NewsView } from '~/components/NewsView';
@@ -8,11 +8,28 @@ import { INews } from '~/models/News';
 export const LocalNewsPage: React.VFC = () => {
   const router = useRouter();
   const { country, pref, city } = router.query;
+  const [category, setCategory] = useState('');
+
+  const categories = {
+    '': 'すべて',
+    crisis: '災害',
+    virus: '感染症',
+    accident: '事故',
+    incident: '事件',
+    children: '児童虐待',
+    drug: '薬物乱用',
+    poverty: '貧困',
+    nikkei: '経済',
+    politics: '政治',
+    sports: 'スポーツ',
+  };
 
   let title = '';
   const param = new URLSearchParams();
   param.append('hasLocation', 'true');
-  param.append('category', 'crisis');
+  if (category.length > 0) {
+    param.append('category', category);
+  }
   if (country) {
     param.append('country', country as string);
     title += country;
@@ -31,6 +48,26 @@ export const LocalNewsPage: React.VFC = () => {
   return (
     <div>
       <h1 css={tw`m-5 text-3xl font-bold`}>{title}</h1>
+      <div>
+        <h2 css={tw`text-xl inline ml-4`}>カテゴリ:</h2>
+        {Object.keys(categories).map((cat) => {
+          return (
+            <div key={cat} css={tw`text-xl inline ml-4`}>
+              <input
+                type='radio'
+                id={cat}
+                value={cat}
+                name={cat}
+                checked={cat === category}
+                onChange={(e) => {
+                  setCategory(e.target.value);
+                }}
+              />
+              <label htmlFor={cat}>{categories[cat]}</label>
+            </div>
+          );
+        })}
+      </div>
       {data?.map((news) => {
         return <NewsView key={news.url} news={news} />;
       })}
