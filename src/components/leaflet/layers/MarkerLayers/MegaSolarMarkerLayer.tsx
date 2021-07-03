290cb59ac91440a01cc8fb5809bc212786c88c54
yuiseki <yuiseki@gmail.com>
2021-07-03 17:06:25 +0900

    add MegaSolarMarkerLayer

---
+++ src/components/leaflet/layers/MarkerLayers/MegaSolarMarkerLayer.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import AbstractMarkerLayer from './AbstractMarkerLayer';
+
+export const MegaSolarLayer = () => {
+  const { data } = useSWR(
+    'http://agora.ex.nii.ac.jp/earthquake/201103-eastjapan/energy/electrical-japan/type/8.json'
+  );
+  const [markers, setMarkers] = useState([]);
+  useEffect(() => {
+    if (data) {
+      const newMarkers = data.map((marker, i) => {
+        const content = (
+          <>
+            <b>{marker.o}</b>
+            <br />
+            <b>{marker.n}</b>
+          </>
+        );
+        return {
+          center: [marker.lat, marker.lng],
+          popupContent: content,
+          id: 'mega-solar-' + i,
+          icon: '/images/solar.png',
+        };
+      });
+      setMarkers(newMarkers);
+    }
+  }, [data]);
+  return (
+    <AbstractMarkerLayer
+      id='river-level-marker-layer'
+      title='メガソーラー発電所'
+      markers={markers}
+    />
+  );
+};
