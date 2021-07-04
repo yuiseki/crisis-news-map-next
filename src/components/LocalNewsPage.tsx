68b7c15756f5d97a044b1ed974a8ee5cf4535102
yuiseki <yuiseki@gmail.com>
2021-07-04 21:59:59 +0900

    fix LocalNewsPage

--- src/components/LocalNewsPage.tsx
+++ src/components/LocalNewsPage.tsx
@@ -1,9 +1,18 @@
+//import { LatLngTuple } from 'leaflet';
+//import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import useSWR from 'swr';
 import tw from 'twin.macro';
 import { NewsView } from '~/components/NewsView';
 import { INews } from '~/models/News';
+
+/*
+const StaticMap = dynamic(() => import('./leaflet/StaticMap'), {
+  ssr: false,
+});
+*/
 
 export const LocalNewsPage: React.VFC = () => {
   const router = useRouter();
@@ -25,6 +34,7 @@ export const LocalNewsPage: React.VFC = () => {
   };
 
   let title = '';
+  //const center = [36.5748441, 139.2394179] as LatLngTuple;
   const param = new URLSearchParams();
   param.append('hasLocation', 'true');
   if (category.length > 0) {
@@ -42,35 +52,47 @@ export const LocalNewsPage: React.VFC = () => {
     param.append('city', city as string);
     title += ', ' + city;
   }
+  title += 'のニュース記事';
 
   const url = '/api/news?' + param.toString();
   const { data } = useSWR<INews[]>(url);
+
   return (
-    <div>
-      <h1 css={tw`m-5 text-3xl font-bold`}>{title}</h1>
+    <>
+      <Head>
+        <title>全国災害情報地図 - {title}</title>
+      </Head>
       <div>
-        <h2 css={tw`text-xl inline ml-4`}>カテゴリ:</h2>
-        {Object.keys(categories).map((cat) => {
-          return (
-            <div key={cat} css={tw`text-xl inline ml-4`}>
-              <input
-                type='radio'
-                id={cat}
-                value={cat}
-                name={cat}
-                checked={cat === category}
-                onChange={(e) => {
-                  setCategory(e.target.value);
-                }}
-              />
-              <label htmlFor={cat}>{categories[cat]}</label>
-            </div>
-          );
+        <h1 css={tw`m-5 text-3xl font-bold`}>{title}</h1>
+        {/**
+        <div css={tw`h-80 w-full my-4`}>
+          <StaticMap zoom={5} center={center} />
+        </div>
+           */}
+        <div>
+          <h2 css={tw`text-xl inline ml-4`}>カテゴリ:</h2>
+          {Object.keys(categories).map((cat) => {
+            return (
+              <div key={cat} css={tw`text-xl inline ml-4`}>
+                <input
+                  type='radio'
+                  id={cat}
+                  value={cat}
+                  name={cat}
+                  checked={cat === category}
+                  onChange={(e) => {
+                    setCategory(e.target.value);
+                  }}
+                />
+                <label htmlFor={cat}>{categories[cat]}</label>
+              </div>
+            );
+          })}
+        </div>
+        {data?.map((news) => {
+          return <NewsView key={news.url} news={news} />;
         })}
       </div>
-      {data?.map((news) => {
-        return <NewsView key={news.url} news={news} />;
-      })}
-    </div>
+    </>
   );
 };
--- src/components/NewsView.tsx
+++ src/components/NewsView.tsx
@@ -22,25 +22,35 @@ export const NewsView: React.VFC<{ news: INews }> = ({
   const categoryPath = `/news/${news.category}`;
 
   return (
-    <div css={tw`m-5`}>
-      <a href={news.url}>
-        <h2 css={tw`text-2xl font-bold`}>{news.ogTitle}</h2>
-        <h3 css={tw`text-2xl`}>{news.url}</h3>
-      </a>
-      <div css={tw`text-xl my-1`}>
-        <span css={tw`mr-1`}>
-          取得日時: {new Date(Date.parse(news.createdAt)).toLocaleString()}
-        </span>
+    <div css={tw`m-5 p-5 bg-gray-200`}>
+      <div>
+        <a
+          href={news.url}
+          css={tw`text-blue-600 visited:text-purple-600`}
+          target='_blank'
+          rel='noreferrer'
+        >
+          <h2 css={tw`text-2xl font-bold`}>{news.ogTitle}</h2>
+          <h3 css={tw`text-xl`}>{news.url}</h3>
+        </a>
+        <div css={tw`text-xl my-1`}>
+          <span css={tw`mr-1`}>
+            取得日時: {new Date(Date.parse(news.createdAt)).toLocaleString()}
+          </span>
+        </div>
+        <div css={tw`text-xl my-1`}>
+          <span css={tw`mr-1`}>
+            場所（推定）: <Link href={locationPath}>{locationPath}</Link>
+          </span>
+        </div>
+        <div css={tw`text-xl my-1`}>
+          カテゴリ（推定）: <Link href={categoryPath}>{news.category}</Link>
+        </div>
+        <div>
+          <span css={tw`text-xl`}>{news.ogDesc}</span>
+          <img css={tw`my-2`} src={news.ogImage} width={350} />
+        </div>
       </div>
-      <div css={tw`text-xl my-1`}>
-        <span css={tw`mr-1`}>
-          場所（推定）: <Link href={locationPath}>{locationPath}</Link>
-        </span>
-      </div>
-      <div css={tw`text-xl my-1`}>
-        カテゴリ（推定）: <Link href={categoryPath}>{news.category}</Link>
-      </div>
-      <div>{news.ogDesc}</div>
     </div>
   );
 };
