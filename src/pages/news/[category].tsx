1c1f83c283f05b82d31d9d494867f1fc9199acbc
yuiseki <yuiseki@gmail.com>
2021-07-10 09:33:28 +0900

    /news/[category] で日本のみを絞り込めるようにする

--- src/pages/news/[category].tsx
+++ src/pages/news/[category].tsx
@@ -1,7 +1,7 @@
 import { LatLngTuple } from 'leaflet';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import tw from 'twin.macro';
 import { NewsView } from '~/components/NewsView';
@@ -14,10 +14,47 @@ const StaticMap = dynamic(() => import('../../components/leaflet/StaticMap'), {
 const NewsListView: React.VFC = () => {
   const router = useRouter();
   const { category } = router.query;
-  const url = `/api/news?category=${category}`;
+  const [url, setUrl] = useState<string | null>(null);
+  const [checked, setChecked] = useState(true);
+  const [country, setCountry] = useState('日本');
+  useEffect(() => {
+    const endpoint = '/api/news?';
+    const params = new URLSearchParams();
+    params.append('limit', '100');
+    params.append('page', '0');
+    // @ts-ignore
+    params.append('category', category);
+    if (country) {
+      params.append('country', country);
+    }
+    setUrl(endpoint + params.toString());
+  }, [category, checked, country]);
+
+  useEffect(() => {
+    if (checked) {
+      setCountry('日本');
+    } else {
+      setCountry(null);
+    }
+  }, [checked]);
+
   const { data } = useSWR<INews[]>(url);
   return (
     <div>
+      <h1 css={tw`m-5 text-3xl font-bold`}>{category} 関連ニュース</h1>
+      <div css={tw`text-xl inline ml-5`}>
+        <input
+          type='checkbox'
+          id='placeCountry'
+          value='japan'
+          name='japan'
+          checked={checked}
+          onChange={(e) => {
+            setChecked(e.target.checked);
+          }}
+        />
+        <label htmlFor='placeCountry'>日本のみ</label>
+      </div>
       {data?.map((news) => {
         const center = [news.latitude, news.longitude] as LatLngTuple;
         return (
