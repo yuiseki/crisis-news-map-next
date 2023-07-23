e0080345c0f1ef01ced6b9e7ef4b750030bd3fcc
yuiseki <yuiseki@gmail.com>
2023-07-23 10:02:48 +0900

    fix localStorage

--- src/components/leaflet/handler/MapInitializer.tsx
+++ src/components/leaflet/handler/MapInitializer.tsx
@@ -6,19 +6,23 @@ export const MapInitializer = () => {
   const [center, setCenter] = useState([36.57142382346277, 132.31701507110336]);
   const [zoom, setZoom] = useState(7);
   useEffect(() => {
-    // center
-    const lat = localStorage.getItem('leaflet-center-lat');
-    const lng = localStorage.getItem('leaflet-center-lng');
-    if (!lat || !lng || lat === '0' || lng === '0') {
-      return;
+    try {
+      // center
+      const lat = localStorage.getItem('leaflet-center-lat');
+      const lng = localStorage.getItem('leaflet-center-lng');
+      if (!lat || !lng || lat === '0' || lng === '0') {
+        return;
+      }
+      setCenter([Number(lat), Number(lng)]);
+      // zoom
+      const zoom = localStorage.getItem('leaflet-zoom');
+      if (!zoom || zoom === '0') {
+        return;
+      }
+      setZoom(Number(zoom));
+    } catch (error) {
+      console.error(error);
     }
-    setCenter([Number(lat), Number(lng)]);
-    // zoom
-    const zoom = localStorage.getItem('leaflet-zoom');
-    if (!zoom || zoom === '0') {
-      return;
-    }
-    setZoom(Number(zoom));
   }, []);
   useEffect(() => {
     // @ts-ignore
